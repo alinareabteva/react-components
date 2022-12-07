f25fcf36f0ca60a5d31cbb351bd5a268dc32ad84
alinareabteva <52407474+alinareabteva@users.noreply.github.com>
2022-12-07 13:24:46 +0200

    Added a progressive bar into icon collection

--- src/components/icon-collection/Sandbox.jsx
+++ src/components/icon-collection/Sandbox.jsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import TabBar from '../tabbar/TabBar';
 import TabBarItem from '../tabbar/TabBarItem';
 import Button from '../button/Button';
-import Icon, {IconNames, IconSizes} from '../icon/icon';
+import Icon, {IconNames, IconSizes, IconProgressBar} from '../icon/icon';
 import './Sandbox.css';
 
 
@@ -33,6 +33,10 @@ const Sandbox = () => {
   const biggestIconSize = IconSizesValues[IconSizesValues.length - 1];
   const lowestIconSize = IconSizesValues[0];
 
+  const [rangeval, setRangeval] = useState(100);
+
+ 
+
 return (
   <>
     <h2 className="text"><span>Icon Collection</span></h2>
@@ -41,12 +45,20 @@ return (
       <Button data-directionindex="-1" onClick={changeSize} disabled={state.iconSize === lowestIconSize}>-</Button>
       <Button data-directionindex="1" onClick={changeSize} disabled={state.iconSize === biggestIconSize}>+</Button>
       <select value={state.iconSize} onChange={onSelectIconSize} >
-      <option key="empty-option" >Select Icon Size</option>
+      <option key="empty-option" disabled >Select Icon Size</option>
         {Object.entries(IconSizes).map(([key, value]) => (
           <option key={key} value={value}>{key}</option>
         ))}
       </select>
+      
+      <input type="range" className="custom-range" min="0" max="100" defaultValue="100"
+       onChange={(event) => setRangeval(event.target.value)} />
+      <output className="percent-text">{rangeval + " %"}</output>
+    
     </section>
+
+   
+
 
     <TabBar vertical>
       {Object.entries(IconNames).map(([key, value]) => (
